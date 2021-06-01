5592d57ac48472149b2ee80b3265f75a44e418ea
Nathalie Loussouarn <natel@hotmail.com>
2021-06-01 07:47:18 +0200

    feat(event) controller test update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,12 +13,20 @@ class EventsController < ApplicationController
 
   def update
     render json: { success: true }
+    @event = Event.find(params[:id])
+    @event_new_position = Event.find(params[:newPosition])
+    raise
+    @event.update(params[:newPosition])
+ 
   end
-
 
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
     redirect_to request.referrer
   end
+
+  private
+
+
 end
