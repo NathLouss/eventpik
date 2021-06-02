ff9672ce9f9c3b5a98dd1f31dcae28bd1674ad60
Lea Brami <leadbrami@gmail.com>
2021-06-02 04:33:38 -0400

    method events calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,18 +5,24 @@ class CalendarsController < ApplicationController
     # itérer sur la liste d'events
     @events.each_with_index do |event, index|
     # si c'est le 1er event on lui attribut le debut de la wish list (start_at)
-      if @events[0]
-        event.start_time = current_user.wish_list.start_at.hour + 10
+      if index == 0
+        event.start_time = current_user.wish_list.start_at + 10.hour
+        event.save
       else
         # si (heure démarrage event previous + (durée activity + 30 mn).formatminutes).récup heure est sup à 18h
-        if (@events[index - 1].start_time + (@events[index - 1].activity.duration + 30).minutes).hour > 18
+        previous = @events[index - 1]
+        if previous.next_hour > 18
           # alors on ajoute 1 jour au démarrage de l'event précédent
-          event.start_time = @events[index - 1].start_time + 1.day
           # et on lui attribut une heure de démarrage
-          event.start_time.hour = 10
+          year = previous.start_time.year
+          month = previous.start_time.month
+          day = previous.start_time.day + 1
+          event.start_time = DateTime.new(year, month, day, 10, 0)
+          event.save
         else
           # sinon on ajoute l'event suivant à la fin l'event précédant et des 30 mn de déplacement
-          event.start_time = @events[index - 1].start_time + (@events[index - 1].activity.duration + 30).minutes
+          event.start_time = previous.next_slot
+          event.save
         end
       end
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,11 @@ class Event < ApplicationRecord
   belongs_to :activity
 
   # validates :start_time, presence: true
+  def next_hour
+   return self.next_slot.hour + self.next_slot.min * 1.0/60
+  end
 
+  def next_slot
+   self.start_time + (self.activity.duration + 30).minute
+  end
 end
