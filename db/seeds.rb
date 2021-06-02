23cf5dfd66ceeb3e9c4bc78ee7e521f96e6abae5
Lea Brami <leadbrami@gmail.com>
2021-06-02 08:32:39 -0400

    add activities - seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,6 +56,78 @@ activity1 = Activity.new(
   activity3.photo.attach(io: URI.open('https://res.cloudinary.com/dfuxvscct/image/upload/v1622024180/degustation-vin_gsnaoq.png'), filename: 'nes.png', content_type: 'image/png')
   activity3.save!
 
+  activity4 = Activity.new(
+  title: "Visite Gastronomique de Bordeaux",
+  category: "gastronomie",
+  address: "Place de la Bourse, 33000 Bordeaux",
+  description: "Après une échappée dans le parc, entrez dans le cuvier bois dédié à la vinification du vin rouge, assorti de ses piges en inox et faites-vous conter ses secrets de fabrication.
+  Pénétrez enfin dans le chai à barriques où repose notre vin pendant 18 mois et découvrez les subtilités d’un élevage réussi.
+  Terminez votre visite par la dégustation de 3 vins de terroirs différents dont le Château Pape Clément rouge.",
+  website_url: "https://www.bordeaux-tourisme.com/gastronomie-art-vivre/",
+  duration: 210,
+  price: 80,
+  rating: 4
+  )
+  activity4.photo.attach(io: URI.open('https://res.cloudinary.com/dfuxvscct/image/upload/v1622636078/degustation-bordeaux_itrwji.png'), filename: 'nes.png', content_type: 'image/png')
+  activity4.save!
+
+activity5 = Activity.new(
+  title: "Sentinelles du climat",
+  category: "culture",
+  address: "5 place Bardineau, 33000 Bordeaux",
+  description: "Cette exposition présente, à l'échelle régionale, les effets du changement climatique sur la biodiversité.
+  Basée sur ces sentinelles, l'exposition présente le contexte actuel de déclin de la biodiversité lié aux activités humaines, dont un focus plus détaillé de la question des impacts de la crise climatique.",
+  website_url: "https://www.bordeaux.fr/agendaGeneral/pgFicheEvt.psml?_nfpb=true&_pageLabel=pgFicheEvt&classofcontent=evenement&id=193662&iEvt=21&retourListe=1&classListe=agendaContribuable&idListe=5955&typeAgendaListe=&nbreEvtPageListe=25/",
+  duration: 90,
+  price: 7,
+  rating: 4
+  )
+  activity5.photo.attach(io: URI.open('https://res.cloudinary.com/dfuxvscct/image/upload/v1622636074/climat_j9ngp2.png'), filename: 'nes.png', content_type: 'image/png')
+  activity5.save!
+
+  activity6 = Activity.new(
+  title: "Balade aérienne au dessous de la région Bordelaise",
+  category: "sport",
+  address: "305 Avenue de Mont de Marsan, 33850 Léognan",
+  description: "Rejoignez un pilote sur un vol unique et partagez le coût avec lui.Une excellente façon de découvrir les magnifiques paysages de la région à bord d'un avion privé.
+  Votre pilote Wingly se fera un plaisir de partager avec vous sa passion pour le vol et vous emmènera pour un voyage inoubliable au-dessus de la région de Bordeaux, vous offrant des vues imprenables sur la dune du Pilat, les vignobles locaux et bien plus encore.",
+  website_url: "https://www.viator.com/fr-CA/tours/France/Discovery-Flight-of-Bordeauxs-Surroundings-with-WIngly/d51-110321P26/",
+  duration: 60,
+  price: 135,
+  rating: 5
+  )
+  activity6.photo.attach(io: URI.open('https://res.cloudinary.com/dfuxvscct/image/upload/v1622636316/balade-aerienne_trbqa2.png'), filename: 'nes.png', content_type: 'image/png')
+  activity6.save!
+
+  activity7 = Activity.new(
+  title: "Bump-Games Bordeaux",
+  category: "sport",
+  address: "16 avenue Louis de Broglie, 33600 Pessac",
+  description: "Découvrez les activités Bump Games à Bordeaux !
+Avec Bubble Bump, Archery Bump et Laser Bump il y en a pour toutes les envies, pour les petits comme pour les grands.
+Éclatez-vous avec les Bump-Games dans la Belle Endormie et partout en Gironde.",
+  website_url: "https://www.bump-games.com/decouvrir-bump-games-a-bordeaux.php/",
+  duration: 90,
+  price: 20,
+  rating: 4
+  )
+  activity7.photo.attach(io: URI.open('https://res.cloudinary.com/dfuxvscct/image/upload/v1622636370/bump-bordeaux_dnzlhm.png'), filename: 'nes.png', content_type: 'image/png')
+  activity7.save!
+
+  activity8 = Activity.new(
+  title: "John Doe escape game",
+  category: "sport",
+  address: "7 Rue d'Alembert, 33000 Bordeaux",
+  description: "Votre cerveau seul ne vous aidera pas, vous devrez courir, crier ou faire des choix pour vous échapper. Vous pensez être seuls dans la salle? Vous croyez pouvoir sortir sans avoir transpiré ?
+  Oubliez tout, aujourd'hui vous allez vivre une expérience unique.",
+  website_url: "https://bordeaux.john-doe.fr/",
+  duration: 90,
+  price: 36,
+  rating: 4
+  )
+  activity8.photo.attach(io: URI.open('https://res.cloudinary.com/dfuxvscct/image/upload/v1622636077/escape-game_hgyzef.png'), filename: 'nes.png', content_type: 'image/png')
+  activity8.save!
+
   puts "Finished !"
 
   puts "create events"
