c6c87b2111959fcd40504d2e25924bc784e32fcb
Lea Brami <leadbrami@gmail.com>
2021-06-02 05:17:28 -0400

    method generate calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,31 +1,7 @@
 class CalendarsController < ApplicationController
   def create
-
-    @events = current_user.wish_list.events
-    # itérer sur la liste d'events
-    @events.each_with_index do |event, index|
-    # si c'est le 1er event on lui attribut le debut de la wish list (start_at)
-      if index == 0
-        event.start_time = current_user.wish_list.start_at + 10.hour
-        event.save
-      else
-        # si (heure démarrage event previous + (durée activity + 30 mn).formatminutes).récup heure est sup à 18h
-        previous = @events[index - 1]
-        if previous.next_hour > 18
-          # alors on ajoute 1 jour au démarrage de l'event précédent
-          # et on lui attribut une heure de démarrage
-          year = previous.start_time.year
-          month = previous.start_time.month
-          day = previous.start_time.day + 1
-          event.start_time = DateTime.new(year, month, day, 10, 0)
-          event.save
-        else
-          # sinon on ajoute l'event suivant à la fin l'event précédant et des 30 mn de déplacement
-          event.start_time = previous.next_slot
-          event.save
-        end
-      end
-    end
+    @events = current_user.wish_list.events.order(:start_time)
+    @events.assign_start_time
     redirect_to calendar_path(1)
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,32 @@ class Event < ApplicationRecord
   belongs_to :activity
 
   # validates :start_time, presence: true
+  def self.assign_start_time
+    events = self.all
+    events.each_with_index do |event, index|
+    # si c'est le 1er event on lui attribut le debut de la wish list (start_at)
+      if index == 0
+        event.start_time =event.wish_list.start_at + 10.hour
+      else
+        # si (heure démarrage event previous + (durée activity + 30 mn).formatminutes).récup heure est sup à 18h
+        previous = events[index - 1]
+        if previous.next_hour > 18
+          # alors on ajoute 1 jour au démarrage de l'event précédent
+          # et on lui attribut une heure de démarrage
+          year = previous.start_time.year
+          month = previous.start_time.month
+          day = previous.start_time.day + 1
+          event.start_time = DateTime.new(year, month, day, 10, 0)
+        else
+          # sinon on ajoute l'event suivant à la fin l'event précédant et des 30 mn de déplacement
+          event.start_time = previous.next_slot
+        end
+      end
+      event.save
+    end
+
+
+  end
   def next_hour
    return self.next_slot.hour + self.next_slot.min * 1.0/60
   end
