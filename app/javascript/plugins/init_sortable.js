f7ba7ba9a686678d492aed9d279ef1a88701abae
Alexandre Badin <alexandre.badin33@gmail.com>
2021-06-03 15:19:50 +0200

    feat(activities#show)rollback to previous front

--- app/javascript/plugins/init_sortable.js
+++ app/javascript/plugins/init_sortable.js
@@ -2,14 +2,37 @@ import Sortable from 'sortablejs';
 import { csrfToken } from "@rails/ujs";
 
 const initSortable = () => {
-  const list = document.querySelector('#results');
-  Sortable.create(list, {
+  const list1 = document.querySelector('#results1');
+  Sortable.create(list1, {
     ghostClass: "ghost",
     animation: 150,
     onEnd: (event) => {
       const eventId = event.clone.dataset.eventId;
-      alert(`${event.oldIndex} moved to ${event.newIndex}`);
-    
+      console.dir(event)
+      fetch(`/events/${eventId}`, {
+        method: "PATCH",
+        headers: {
+          "X-CSRF-Token": csrfToken(),
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        credentials: "same-origin",
+        body: JSON.stringify({ newPosition: event.newIndex, oldPosition: event.oldIndex }),
+      })
+      .then(response => response.json())
+      .then((data) => {
+        document.location.reload();
+      });
+    }
+  });
+
+  const list2 = document.querySelector('#results2');
+  Sortable.create(list2, {
+    ghostClass: "ghost",
+    animation: 150,
+    onEnd: (event) => {
+      const eventId = event.clone.dataset.eventId;
+     
       fetch(`/events/${eventId}`, {
         method: "PATCH",
         headers: {
